00495194e4cf4598728dc006767661dedc45814e
kath1987 <144858782+kath1987@users.noreply.github.com>
2024-01-25 16:45:10 -0400

    Agregar proyecto de creación de control remoto

---
+++ Proyectos Web (HTML y CSS)/Control_remoto/java.js
@@ -0,0 +1,8 @@
+// jaba/java.js
+
+// Ejemplo de función para el botón Power
+function myFuncion() {
+    var powerElement = document.querySelector(".Power");
+    powerElement.innerHTML = "N E C F L I Z";
+    // Agrega aquí cualquier otra lógica que desees ejecutar al hacer clic en el botón Power
+}
